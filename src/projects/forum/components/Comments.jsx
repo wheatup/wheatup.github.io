ac0fe91b8a55d9e0fcde5a4f00e4783aaa2caf61
wheatup <acphun@gmail.com>
2020-08-28 17:36:04 +0900

    more fix

--- src/projects/forum/components/Comments.jsx
+++ src/projects/forum/components/Comments.jsx
@@ -5,24 +5,39 @@ import UserAvatar from './UserAvatar';
 import { useMemo } from 'react';
 import Markdown from './Markdown';
 import { getTimeDiff } from '../utils/misc';
+import { useCallback } from 'react';
+import { useState } from 'react';
+
 
 const Comments = ({ comments }) => {
+
+
+
+	let lastTime = 0;
+	const shouldShowTime = time => {
+		if (time - lastTime > 60 * 1000) {
+			lastTime = time;
+			return true;
+		}
+	};
+
+	const me = useData(ME);
+	const isMe = useCallback(user => me && me.id === user.id);
+
 	return (
 		<div className="Comments">{comments.map(comment => (
 			<div key={comment.id.toString()} className="comment-wrapper">
-				<time title={new Date(comment.created_at).toLocaleString('zh-CN', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })}>{getTimeDiff(comment.created_at)}</time>
-				<Comment comment={comment} />
+				{shouldShowTime(new Date(comment.created_at).getTime()) && <time className={isMe(comment.user) ? 'me' : ''} title={new Date(comment.created_at).toLocaleString('zh-CN', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })}>{getTimeDiff(comment.created_at)}</time>}
+				<Comment comment={comment} isMe={isMe(comment.user)} />
 			</div>
 		))}
 		</div>
 	);
 }
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, isMe }) => {
 	const { body, user } = comment;
-	const me = useData(ME);
-	const isMe = useMemo(() => me && me.id === user.id);
-
+	
 	return (
 		<div className={`Comment${isMe ? ' me' : ''}`}>
 			<UserAvatar user={user} />
