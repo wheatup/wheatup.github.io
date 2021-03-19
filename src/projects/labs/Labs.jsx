fa76294fc5bbd5f55cd8c5639d2858ecf6c3378b
wheatup <acphun@gmail.com>
2021-03-19 21:37:53 +0900

    fix

--- src/projects/labs/Labs.jsx
+++ src/projects/labs/Labs.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import LinearRegression from './laboratories/linearRegression/LinearRegression';
-import TensorFlow from './laboratories/linearRegression/TensorFlow';
+// import LinearRegression from './laboratories/linearRegression/LinearRegression';
+// import TensorFlow from './laboratories/linearRegression/TensorFlow';
 import Idioms from './laboratories/idioms/Idioms';
 import HexieEncoder from './laboratories/hexie/HexieEncoder';
 import $$ from 'whi18n';
@@ -15,16 +15,16 @@ const Labs = props => {
 					{/* <NavLink to="/labs/tensorflow">Tensor Flow</NavLink> */}
 					<NavLink to="/labs/hexie">{$$`hexie-encoder`}</NavLink>
 					<NavLink to="/labs/idioms">{$$`idioms`}</NavLink>
-					<NavLink to="/labs/linear-regression">Linear Regression</NavLink>
+					{/* <NavLink to="/labs/linear-regression">Linear Regression</NavLink> */}
 				</div>
 			</div>
 			<div className="laboratory">
 				<Switch>
-					<Route path="/labs/tensorflow" component={TensorFlow} />
+					{/* <Route path="/labs/tensorflow" component={TensorFlow} /> */}
 					<Route path="/labs/hexie" component={HexieEncoder} />
 					<Route path="/labs/idioms" component={Idioms} />
-					<Route path="/labs/linear-regression" component={LinearRegression} />
-					<Redirect to="/labs/linear-regression" />
+					{/* <Route path="/labs/linear-regression" component={LinearRegression} /> */}
+					<Redirect to="/labs/idioms" />
 				</Switch>
 			</div>
 		</section>
