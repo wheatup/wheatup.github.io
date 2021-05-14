5e4db799f8f57850cf86d44f4edc2c2c6f15167b
wheatup <acphun@gmail.com>
2021-05-14 16:52:17 +0900

    blogs

--- src/components/common/Markdown.jsx
+++ src/components/common/Markdown.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { nord as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import gfm from 'remark-gfm';
 
 const components = {
 	code({ node, inline, className, children, ...props }) {
 		const match = /language-(\w+)/.exec(className || '');
-		return !inline && match ? <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} /> : <code className={className} {...props} />;
+		return !inline && match ? <SyntaxHighlighter style={style} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} /> : <code className={className} {...props} />;
 	}
 };
 
 const Markdown = ({ children, ...rest }) => {
 	return (
 		<div className="Markdown">
-			<ReactMarkdown components={components} children={children} {...rest} />
+			<ReactMarkdown remarkPlugins={[gfm]} components={components} children={children} {...rest} />
 		</div>
 	);
 };
