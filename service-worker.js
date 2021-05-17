87f92ad92c85502cad111c5fc0db65a22b724644
wheatup <acphun@gmail.com>
2021-05-17 12:19:08 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bceecf380239b93beff86f1d5a97ba26.js"
+  "/precache-manifest.e0316aab425f8de6af51baee4250a599.js"
 );
 
 self.addEventListener('message', (event) => {
