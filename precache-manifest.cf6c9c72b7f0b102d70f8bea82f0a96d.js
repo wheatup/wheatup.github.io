41f0de7d6e5a0e53da4337de55e4a5ecbecbe4a8
wheatup <acphun@gmail.com>
2021-09-15 15:33:13 +0900

    Updates

---
+++ precache-manifest.cf6c9c72b7f0b102d70f8bea82f0a96d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e3717b0a392edcdb45c0d6f2ef12ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7bdc992d73476abe1c",
+    "url": "/static/css/2.381f00bd.chunk.css"
+  },
+  {
+    "revision": "a14dbbb86f213643d376",
+    "url": "/static/css/main.81baf52d.chunk.css"
+  },
+  {
+    "revision": "2e7bdc992d73476abe1c",
+    "url": "/static/js/2.062f275e.chunk.js"
+  },
+  {
+    "revision": "7d2742fe3cf46b54f9882087cfe011ae",
+    "url": "/static/js/2.062f275e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14dbbb86f213643d376",
+    "url": "/static/js/main.5535bbc1.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  }
+]);
